917a8babd95f989214a43b8114844573272b42e7
shadowninja55 <shadowninja1050@gmail.com>
2022-12-23 14:23:35 -0500

    switch to using dfs, fix dictionary

--- boggler.rb
+++ boggler.rb
@@ -8,33 +8,27 @@ print "letters: "
 BOARD = gets.chomp.chars.map.with_index { |c, i| [Complex(i % 4, i / 4), c] }.to_h
 DIRECTIONS = [0-1i, 1-1i, 1+0i, 1+1i, 0+1i, -1+1i, -1+0i, -1-1i]
 
-def search start
-  stack = [[[start], BOARD[start]]]
+def search path, word
   solutions = []
-  until stack.empty? do
-    path, word = stack.pop 
-    solutions.push [path, word] if DICTIONARY.has_key?(word)
-    next if DICTIONARY.children(word).empty?
-    curr = path.last
-    DIRECTIONS.each do |direction|
-      neighbor = curr + direction
-      stack.push [path + [neighbor], word + BOARD[neighbor]] if BOARD.has_key? neighbor and not path.include? neighbor
-    end
-  end
+  solutions << [path, word] if DICTIONARY.has_key? word
+  return solutions if DICTIONARY.children(word).empty?
+  solutions += (DIRECTIONS.map { path.last + _1 } - path)
+    .intersection(BOARD.keys)
+    .flat_map { search(path + [_1], word + BOARD[_1]) }
   solutions
 end
 
 def highlight path, word
-  board = BOARD.clone
+  canvas = BOARD.clone
   path.each do |point|
-    board[point] = board[point].colorize(:green)
+    canvas[point] = canvas[point].colorize(:green)
   end
-  word + "\n\n" + board.values.each_slice(4).map { _1.join(" ") }.join("\n")
+  word + "\n\n" + canvas.values.each_slice(4).map { _1.join(" ") }.join("\n")
 end
 
-solutions = BOARD.keys
-  .flat_map { search _1 }
+BOARD.keys
+  .flat_map { search [_1], BOARD[_1] }
   .reject { _1.last.length < 3 }
   .uniq(&:last)
   .sort_by { _1.last.length }
-solutions.each { puts highlight(*_1) + "\n\n" }
+  .each { puts highlight(*_1) + "\n\n" }
